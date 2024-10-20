aca5f7e92f8b81b708dfde118b15a7d65899948d
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-20 13:40:39 -0400

    s

--- addons.js
+++ addons.js
@@ -56,6 +56,7 @@ var selectors = [
 ];
 
 setTimeout(() => {
+    document.title = "Penguin Paint - Addons";
     selectors.forEach(selector => {
         const element = document.querySelector(selector);
         if (element) {
