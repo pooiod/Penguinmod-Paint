a4dc6d8ce36c0d74583c3bda24c6254667c6ffd9
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-19 23:00:53 -0400

    h

--- addons.js
+++ addons.js
@@ -1,5 +1,56 @@
 var selectors = [
-    "#app > div > div.settings_header_Y9USz"
+    "#app > div > div.settings_header_Y9USz",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(3)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(5)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(6)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(4)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(5)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(6)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(7)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(8)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(9)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(10)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(12)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(13)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(14)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(15)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(17)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(18)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(25)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(26)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(27)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(28)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(29)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(30)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(31)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(32)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(33)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(34)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(36)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(37)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(38)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(39)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(40)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(41)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(42)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(44)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(45)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(46)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(48)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(49)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(52)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(53)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(54)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(55)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(56)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(57)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(58)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(59)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(60)",
+    "#app > div > div.settings_addons_2LLFF > div > div:nth-child(1) > div:nth-child(3)"
 ];
 
 setTimeout(() => {
@@ -9,4 +60,27 @@ setTimeout(() => {
             element.style.display = 'none';
         }
     });
+}, 100); 
+
+setTimeout(() => {
+    selectors.forEach(selector => {
+        const element = document.querySelector(selector);
+        if (element) {
+            element.style.display = 'none';
+        }
+    });
 }, 500); 
+
+setTimeout(() => {
+    const addonsElement = document.querySelector('#app > div > div.settings_addons_2LLFF > div > footer > span');
+    if (addonsElement && element.textContent !== "Addons version 7fcab09") {
+        addonsElement.textContent = "Addons out of date";
+    }
+    
+    selectors.forEach(selector => {
+        const element = document.querySelector(selector);
+        if (element) {
+            element.style.display = 'none';
+        }
+    });
+}, 1000); 
