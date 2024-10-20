ad58f3aadb3cde933572006230841ced5d8d1c68
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-20 13:58:15 -0400

    h

--- addons.js
+++ addons.js
@@ -83,7 +83,7 @@ setTimeout(() => {
 
 setTimeout(() => {
     const addonsElement = document.querySelector('#app > div > div.settings_addons_2LLFF > div > footer > span');
-    if (addonsElement && element.textContent !== "Addons version 7fcab09") {
+    if (addonsElement && addonsElement.textContent !== "Addons version 7fcab09") {
         addonsElement.textContent = "Addons out of date";
     } else {
         addonsElement.innerHTML = "Penguin Paint is <a src='https://github.com/pooiod/Penguinmod-Paint'>open source</a>!";
