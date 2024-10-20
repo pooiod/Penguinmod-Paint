a165ec43586d4190304fa3d246034e07520836c5
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-19 22:39:50 -0400

    o

--- addons.js
+++ addons.js
@@ -1,10 +1,12 @@
-const selectors = [
+var selectors = [
     "#app > div > div.settings_header_Y9USz"
 ];
 
-selectors.forEach(selector => {
-    const element = document.querySelector(selector);
-    if (element) {
-        element.style.display = 'none';
-    }
-});
+setTimeout(() => {
+    selectors.forEach(selector => {
+        const element = document.querySelector(selector);
+        if (element) {
+            element.style.display = 'none';
+        }
+    });
+}, 500); 
