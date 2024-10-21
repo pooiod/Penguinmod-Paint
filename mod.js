b67b5344d48932603a8896db82953a39bba4365c
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-21 07:16:42 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 var newtitle = "Penguin Paint";
 document.title = newtitle;
 
-// Fun fact: you can load into penguinpaint on penguinmod by loading it as an unsandboxed extension
+// Fun fact: you can load into penguinpaint on penguinmod by loading it as an unsandboxed extension (no addon support)
 
 window.stageWidth = 480;
 window.stageHeight = 360;
@@ -32,6 +32,10 @@ function extrabuttons() { document.title = newtitle;
         button.addEventListener('click', function() {
             window.open("https://penguinpaint.pages.dev/addons", "_blank", "width=520,height=700,left=" + (screen.width / 2 - 250) + ",top=" + (screen.height / 2 - 350));
         });
+
+        if (window.location.hostname === "studio.penguinmod.com") {
+            document.getElementById("addonsbutton").style.display= "none";
+        }
 
         buttonHTML = '<button class="settings_button_2ovv0 buttonsize" style="background: #00c3ff; color: #fff; border: none; border-radius: 5px; padding: 10px;"><b>Set canvas size</b></button>';
         targetElement.insertAdjacentHTML('afterend', buttonHTML);
