98adb9bb82668a4f2e906842f42905c02985fdce
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-21 13:27:35 -0400

    l

--- mod.js
+++ mod.js
@@ -309,7 +309,7 @@ function extrabuttons() { document.title = newtitle;
     }
 }
 
-fatalError = function(err) {
+fatalErrorInt = function(err) {
     err = err || "Error: unknown error"
     var error = `<b>Penguin Paint had an unexpected fatal error, and could not recover</b><br>${err}`;
     document.body.innerHTML = `<div style="display:flex;flex-direction:column;align-items:center;justify-content:center;height:100vh;background-color:#fff;font-size:24px;color:#00a6d9;">${error}</div>`;
